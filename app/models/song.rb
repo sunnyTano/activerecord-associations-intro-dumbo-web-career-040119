4e8509e7bed97f771ef4a14542b9732c06b7f13f
Susan Tan <ssusantan9@gmail.com>
2019-04-14 21:28:09 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,3 +4,10 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
 end
+
+
+# someone_like_you = Song.new(name: "Someone Like You")
+# someone_like_you.artist = adele
+
+# rolling_in_the_deep = Song.new(name: "Rolling in the Deep")
+# adele.songs 
